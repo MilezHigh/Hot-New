b192d3f9ced4f0107284f96d79a6bf53c701c79b
MilezHigh <miles.fishman@yahoo.com>
2022-10-12 15:11:10 -0700

    Remove print statement

--- HotNewRestaurants/YelpDetailsMVVM/YelpDetailsViewModel.swift
+++ HotNewRestaurants/YelpDetailsMVVM/YelpDetailsViewModel.swift
@@ -40,7 +40,6 @@ class YelpDetailsViewModel {
                     print(error.localizedDescription)
                 }
             } receiveValue: { [weak self] models in
-                print(models.count)
                 self?.reviews = models[0..<3].map({ $0 })
             }
             .store(in: &subscriptions)
